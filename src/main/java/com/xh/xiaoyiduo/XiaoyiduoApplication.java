9cf48525492af3d09bfbd9ef51cca9973c09878d
guyuanhui <gyhisme@foxmail.com>
2018-11-15 14:00:45 +0800

    创建SpringBoot项目

---
+++ src/main/java/com/xh/xiaoyiduo/XiaoyiduoApplication.java
@@ -0,0 +1,13 @@
+package com.xh.xiaoyiduo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaoyiduoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaoyiduoApplication.class, args);
+        System.out.println("成功启动");
+    }
+}
